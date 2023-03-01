a7647f964d459c475519d7282462b3b2a611f304
Maik <maikew2006@gmail.com>
2023-03-01 11:19:00 +0100

    Test

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <div class="background">
+        <header>
+            <div class="logo-img header">
+                <img src="img/Sushi-Logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">RESERVATIONS</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                    <button>
+                        <a href="order.php">ORDER</a>
+                    </button>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <div class="box"></div>
+    <!-- <div class="logo-img">
+        <img src="https://cdn.domestika.org/c_fit,dpr_auto,f_auto,t_base_params,w_820/v1618249105/content-items/007/651/207/Datovy%25CC%2581%2520zdroj%25203-original.png?1618249105"
+            alt="">
+    </div> -->
+    <!-- <footer></footer> -->
+
+    <?php 
+    //database magie
+    ?>
+    <script scr="js/media.js"></script>
+</body>
+
+</html>
